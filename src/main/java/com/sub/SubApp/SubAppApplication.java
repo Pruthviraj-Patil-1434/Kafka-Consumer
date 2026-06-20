a20c526d11873a1744545c61d170ed5f315c7229
Pruthviraj-Patil-1434 <pruthvirajpatil1434@gamil.com>
2026-06-20 22:52:11 +0530

    Kafka Consumer

---
+++ src/main/java/com/sub/SubApp/SubAppApplication.java
@@ -0,0 +1,13 @@
+package com.sub.SubApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sub/SubApp/SubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sub.SubApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
